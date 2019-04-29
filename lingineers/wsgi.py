7eaed4763549c92e9f5ad6592db02e1bf6a6e7ea
Yu-Ming Chang <z1e2r3o4o5n6e7@gmail.com>
2019-04-29 15:20:07 +0800

    init lingineers

--- lingineers/wsgi.py
+++ lingineers/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for linguists project.
+WSGI config for lingineers project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'linguists.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lingineers.settings')
 
 application = get_wsgi_application()
