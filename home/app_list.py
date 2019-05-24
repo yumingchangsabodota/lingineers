a3fa77ac88800f026b59d5717a48818efdd64b99
Yu-Ming Chang <z1e2r3o4o5n6e7@gmail.com>
2019-05-24 13:36:36 +0800

    modify some layout

--- home/app_list.py
+++ home/app_list.py
@@ -1,6 +1,6 @@
 app_list = [
-{'app_name':'Vowel Chart', 'url':'/vowelchart/'},
-{'app_name':'Syntax Tree', 'url':'/vowelchart/'},
-{'app_name':'FFT', 'url':'/FFT/'},
+{'app_name':'Vowel Chart', 'url':'/vowelchart/', 'icon':'vowelchart.svg',},
+{'app_name':'Syntax Tree', 'url':'/vowelchart/', 'icon':'syntaxtree.svg'},
+{'app_name':'FFT', 'url':'/FFT/', 'icon':'FFT.svg'},
 
 ]
