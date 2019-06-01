69dbe69abbc00ea82a7f397676598ee495dccca3
Yu-Ming Chang <z1e2r3o4o5n6e7@gmail.com>
2019-06-01 12:15:14 +0800

    change icon

--- home/app_list.py
+++ home/app_list.py
@@ -1,5 +1,5 @@
 app_list = [
-{'app_name':'Vowel Space', 'url':'/vowelchart/', 'icon':'vowelchart.svg',}
+{'app_name':'Vowel Space', 'url':'/vowelchart/', 'icon':'General_American_monophthong_chart.svg',}
 
 ]
 #{'app_name':'Syntax Tree', 'url':'/vowelchart/', 'icon':'syntaxtree.svg'},
